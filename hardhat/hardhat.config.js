ed98e7bc574e8a9061c05fb3e9025f6cd014edae
carlsins <96352590+Haikalmulop@users.noreply.github.com>
2022-05-29 08:52:42 +0800

    changes2

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config({ path: ".env"});
+require("dotenv").config({ path: ".env" });
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 module.exports = {
@@ -9,9 +10,8 @@ module.exports = {
   networks: {
     rinkeby: {
       url: ALCHEMY_API_KEY_URL,
-      accounts: [RINKEBY_PRIVATE_KEY]
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
-
 };
 
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,30 +1,31 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
   so whitelistContract here is a factory for instances of our Whitelist contract.
   */
-    const whitelistContract = await ethers.getContractFactory("whitelist");
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-    //deploy contract
-    //10 is the maximum number of whitelisted addresses
-    const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
+  
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-    //wait for finish deploying
-    await deployedWhitelistContract.deployed();
-
-    //print deployed contract
-    console.log("whitelist contract addresses;", deployedWhitelistContract.address);
-
+  // print the address of the deployed contract
+  console.log(
+    "Whitelist Contract Address:",
+    deployedWhitelistContract.address
+  );
 }
 
-//call the main function
-    main()
-    .then (() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
